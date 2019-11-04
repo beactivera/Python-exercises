eaecb33af39f152de48b32e711c65b2207ba49f0
beactivera <weronika.wdowiak56@gmail.com>
2019-11-04 16:39:20 +0100

    zadanie 2

--- 6_while.py
+++ 6_while.py
@@ -1,6 +1,17 @@
 import math
 
-liczba = int(input("podaj liczbe: "))
-while math.sqrt(liczba) * math.sqrt(liczba)==liczba:
-    print('Jest to kwadrat liczby naturalnej')
-    break
+#zad1
+# liczba = int(input("podaj liczbe: "))
+# while math.sqrt(liczba) * math.sqrt(liczba)==liczba:
+#     print('Jest to kwadrat liczby naturalnej')
+#     break
+
+#zad2
+# k=int(input("podaj liczbe naturalna: "))
+# n=0
+# while pow(2,n)<=k:
+#     m=pow(2,n)
+#     print(m)
+#     n+=1
+
+   
