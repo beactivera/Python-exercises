e4c5e1d9c15938b2a417b06baf08a5f9670656df
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-09 22:01:10 +0100

    zadanie 4

--- extra/python2.py
+++ extra/python2.py
@@ -1,17 +1,24 @@
 #petle
 
-n = int(input('podaj poczatkowa liczbe zakresu: '))
-m = int(input('podaj koncowa liczbe zakresu: '))
-#przedzial rosnacy
-# for i in range(n,m+1):
-#     print(i)
-#przedzial malejacy
-# for i in range(n, m-1, -1):
-#     print(i)
-#przedzial uniwersalny
-if n<m:
-    for i in range(n,m+1):
-        print(i)
-else:
-    for i in range(n, m-1, -1):
-         print(i)
+#zad3
+# n = int(input('podaj poczatkowa liczbe zakresu: '))
+# m = int(input('podaj koncowa liczbe zakresu: '))
+# #przedzial rosnacy
+# # for i in range(n,m+1):
+# #     print(i)
+# #przedzial malejacy
+# # for i in range(n, m-1, -1):
+# #     print(i)
+# #przedzial uniwersalny
+# if n<m:
+#     for i in range(n,m+1):
+#         print(i)
+# else:
+#     for i in range(n, m-1, -1):
+#          print(i)
+
+#zad4
+suma=0
+for i in range(0,101):
+    suma += i
+print(suma)
