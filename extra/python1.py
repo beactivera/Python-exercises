5415534a11827edeffecbb026227518d89d45d35
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-09 21:29:17 +0100

    zadanie 10,11,12

---
+++ extra/python1.py
@@ -0,0 +1,35 @@
+# zmienne, typy, print, input, if
+
+#zad10
+# f=2.314
+# g=65.45
+
+# calf = int(f)%10
+# calg = int(g)%100
+# print(calf,calg)
+
+# ref = int(f)%10
+# reg = int(g)%100
+# print(ref,reg)
+
+# newf= calg + ref
+# newg= calf + reg
+# print(newf, newg)
+
+#zad11
+# i = 3
+# j = 2
+# if i**j > j**i:
+#     print(i, " do ", j, " jest rowne ", i**j, " jest wieksze od ", j, " do ", i, " rowne ", j**i )
+# else:
+#     print(i, " do ", j, " jest rowne ", i**j," jest mniejsze od ", j, " do ", i, " rowne ", j**i )
+
+#zad12
+#pierwiastek z dwoch
+print(2.**(1.0/2.0))
+#pierwiastek trzeciego stopnia z trzech
+print(3.**(1.0/3.0))
+#pierwiastek piatego stopnia z pieciu
+print(5.**(1.0/5.0))
+#pierwiastek n-tego stopnia z pieciu
+print(5.**(1.0,n))
