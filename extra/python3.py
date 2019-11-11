067ecf2d906f797ccb49a53e475d18c12bef8170
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 15:21:23 +0100

    zadanie 4

--- extra/python3.py
+++ extra/python3.py
@@ -1,6 +1,16 @@
 #zad3 
+# tab = [[0 for col in range(10)] for row in range(10)]
+# for i in range(1,11,1):
+#     for y in range(1,11,1):
+#         tab[i-1][y-1] = "*"
+#     print(tab[i-1])
+
+#zad4
 tab = [[0 for col in range(10)] for row in range(10)]
 for i in range(1,11,1):
     for y in range(1,11,1):
-        tab[i-1][y-1] = "*"
+        tab[0][y-1] = "*"
+        tab[i-1][0] = '*'
+        tab[9][y-1] = '*'
+        tab[i-1][9] = '*'
     print(tab[i-1])
