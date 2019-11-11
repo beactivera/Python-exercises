00da0689dda0c2aec9841b36150ddea8e173b5d6
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 19:57:59 +0100

    zadnie 2

--- extra/python7.py
+++ extra/python7.py
@@ -1,9 +1,21 @@
 # LICZBY PSEUDOLOSOWE
 from random import randint
+
+#zad1
+# l = []
+# for i in range(10):
+#     l += [randint(20,100)]
+# print(l)
+# lmin = min(l)
+# lmax = max(l)
+# print('min = ', lmin, 'max = ', lmax)
+
+#zad2
 l = []
 for i in range(10):
-    l += [randint(20,100)]
+    l += [randint(-5,5)]
 print(l)
-lmin = min(l)
-lmax = max(l)
-print('min = ', lmin, 'max = ', lmax)
+suma = 0
+for x in range(len(l)):
+    suma += l[x]
+print('suma tych elementow wynosi: ', suma)
