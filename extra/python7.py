b944d88a1bec5179ebcf3ad33a8ea1ae0d8a2552
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 20:12:09 +0100

    zadanie 3

--- extra/python7.py
+++ extra/python7.py
@@ -11,11 +11,28 @@ from random import randint
 # print('min = ', lmin, 'max = ', lmax)
 
 #zad2
-l = []
-for i in range(10):
-    l += [randint(-5,5)]
-print(l)
-suma = 0
-for x in range(len(l)):
-    suma += l[x]
-print('suma tych elementow wynosi: ', suma)
+# l = []
+# for i in range(10):
+#     l += [randint(-5,5)]
+# print(l)
+# suma = 0
+# for x in range(len(l)):
+#     suma += l[x]
+# print('suma tych elementow wynosi: ', suma)
+
+#zad3
+n = randint(1,1000)
+print(n)
+x = int(input('Podaj wylosowane liczbe: '))
+ilosc = 1
+while x != n:
+    if x < n:
+        print('wylosowane liczba jest wieksza od podanej')
+        ilosc += 1
+        x = int(input('Podaj wylosowane liczbe: '))
+    elif x > n:
+        print('wylosowana liczba jest mniejsza od podanej')
+        ilosc+=1
+        x = int(input('Podaj wylosowane liczbe: '))
+
+print('Brawo, udalo Ci sie znalezc liczbe za ', ilosc, ' razem')
