81e583a2ebfa49fb396b28505c90302f87a81cff
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 18:59:24 +0100

    zadanie 2

--- extra/python6.py
+++ extra/python6.py
@@ -7,12 +7,18 @@
 # print(wynik)
 
 #zad1
-s='Ala ma kota'
-ls =list(s)
-print(ls)
+# s='Ala ma kota'
+# ls =list(s)
+# print(ls)
 
-for i in range(len(ls)):
-    print(ls[i])
+# for i in range(len(ls)):
+#     print(ls[i])
 
-for i in ls:
-    print(ls[i])
+# for i in ls:
+#     print(ls[i])
+
+#zad2
+s = raw_input('Podaj zdanie: ')
+ls = list(s)
+ile = ls.count('a')
+print(ile)
