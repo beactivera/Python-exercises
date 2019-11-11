861ac3aa1cf056b7f4bc98575398f8f1338dc0d5
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 18:54:35 +0100

    zadanie 1

--- extra/python6.py
+++ extra/python6.py
@@ -1,5 +1,18 @@
+#LISTY 2
+
 #zad6
-wynik=1
-for i in range(1,1001):
-    wynik*=i
-print(wynik)
+# wynik=1
+# for i in range(1,1001):
+#     wynik*=i
+# print(wynik)
+
+#zad1
+s='Ala ma kota'
+ls =list(s)
+print(ls)
+
+for i in range(len(ls)):
+    print(ls[i])
+
+for i in ls:
+    print(ls[i])
