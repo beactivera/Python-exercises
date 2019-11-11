8623a4a2e49750242e61304d4dfe739c2a0f92c2
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 15:51:02 +0100

    zadanie 6

--- extra/python3.py
+++ extra/python3.py
@@ -16,11 +16,23 @@
 #     print(tab[i-1])
 
 #zad5
+# tab = [[0 for col in range(10)] for row in range(10)]
+# for i in range(1,11,1):
+#     for y in range(1,11,1):
+#         if i == y:
+#             tab[i-1][y-1] = "*"
+#         else:
+#             tab[i-1][y-1] = ' '
+#     print(tab[i-1])
+
+#zad6
 tab = [[0 for col in range(10)] for row in range(10)]
 for i in range(1,11,1):
     for y in range(1,11,1):
-        if i == y:
-            tab[i-1][y-1] = "*"
+        if i%2 == 0 and y%2 == 0:
+            tab[i-1][y-1] = '*'
+        elif i%2 != 0 and y%2 != 0:
+            tab[i-1][y-1] = '*'
         else:
             tab[i-1][y-1] = ' '
     print(tab[i-1])
