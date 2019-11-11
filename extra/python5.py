e0bac8c25d9f88bdf2078ac6cc0bde23fe87d78f
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 17:21:14 +0100

    zadnie2

--- extra/python5.py
+++ extra/python5.py
@@ -2,21 +2,33 @@
 import copy
 
 #zad1
-l = [3,'alfa', 2.71, 'kot']
-# print(l[-1])
-# dla indexu < 0 , liczy elementy od tylu
-# dla indexu > 0 i indexu > len(lista) program wyrzuci blad
-# l[0] = 4
-# l[len(l)-1] = 'pies'
+# l = [3,'alfa', 2.71, 'kot']
+# # print(l[-1])
+# # dla indexu < 0 , liczy elementy od tylu
+# # dla indexu > 0 i indexu > len(lista) program wyrzuci blad
+# # l[0] = 4
+# # l[len(l)-1] = 'pies'
+# # print(l)
+
+# l2=l # to nie jest kopie tylko odnosnik-reference do starej listy
+# l2[2]=3
 # print(l)
+# print(l2)
 
-l2=l # to nie jest kopie tylko odnosnik-reference do starej listy
-l2[2]=3
-print(l)
-print(l2)
+# # l3=list(l)
+# l3=copy.copy(l)
+# l3[1]='beta'
+# print(l)
+# print(l3)
 
-# l3=list(l)
-l3=copy.copy(l)
-l3[1]='beta'
+#zad2
+l = []
+# for i in range(10):
+#     l = l + [i**2]
+# print(l)
+for i in range(10):
+    if i%2 == 0:
+        l = l + [-(i**2)]
+    else:
+        l = l + [i**2]
 print(l)
-print(l3)
