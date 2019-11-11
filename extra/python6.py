442b8258afd413724523fc9dc11cfbb98b760d9a
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-11-11 19:19:27 +0100

    zadanie 3

--- extra/python6.py
+++ extra/python6.py
@@ -18,7 +18,30 @@
 #     print(ls[i])
 
 #zad2
-s = raw_input('Podaj zdanie: ')
-ls = list(s)
-ile = ls.count('a')
-print(ile)
+# s = raw_input('Podaj zdanie: ')
+# ls = list(s)
+# ile = ls.count('a')
+# print(ile)
+
+#zad3
+i = 321
+si = str(i)
+ls = list(si)
+from math import factorial
+# print(factorial(4)) #silnia
+n = factorial(1000)
+sn = str(n)
+ln = list(sn)
+# print(ln)
+ile0 = ln.count('0')
+ile1 = ln.count('1')
+ile2 = ln.count('2')
+ile3 = ln.count('3')
+ile4 = ln.count('4')
+ile5 = ln.count('5')
+ile6 = ln.count('6')
+ile7 = ln.count('7')
+ile8 = ln.count('8')
+ile9 = ln.count('9')
+print('ile0: ', ile0,'ile1: ', ile1,'ile2: ', ile2,'ile3: ', ile3,'ile4: ', ile4,'ile5: ', ile5,'ile6: ', ile6,'ile7: ', ile7,'ile8: ', ile8,'ile9: ', ile9)
+
