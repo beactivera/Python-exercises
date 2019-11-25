2530049ce1d042bf27f73f517155f8cf964e1f8c
beactivera <weronika.wdowiak56@gmail.com>
2019-11-25 17:07:19 +0100

    zadanie 3

--- 7_pliki.py
+++ 7_pliki.py
@@ -5,13 +5,28 @@ import linecache
 # print(text)
 
 #zad2
+
+# text = open('cytaty.txt','r').read()
+# # text.encode='utf-8'
+# print(text)
+# # ilosc_wierszy = len(open('cytaty.txt').readlines())
+# lines = text.split('\n')
+# # print(lines)
+
+# for i in range(len(lines)):
+#     if i%2!=0:
+#        print(lines[i])
+
+#zad3
 text = open('cytaty.txt','r').read()
 # text.encode='utf-8'
-print(text)
+# print(text)
 # ilosc_wierszy = len(open('cytaty.txt').readlines())
 lines = text.split('\n')
 # print(lines)
 
-for i in range(len(lines)):
-    if i%2!=0:
-       print(lines[i])
+a = int(input("podaj poczatek zakresu: "))
+b = int(input("podaj koniec zakresu: "))
+
+for i in range(a-1,b+1):
+    print(lines[i])
