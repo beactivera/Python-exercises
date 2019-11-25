3bcf425a21f5d267afe3031c8c27b436b1bc7c06
beactivera <weronika.wdowiak56@gmail.com>
2019-11-25 17:03:11 +0100

    zadanie 2

--- 7_pliki.py
+++ 7_pliki.py
@@ -1,4 +1,17 @@
+import linecache
 #zad1
 
-text = open('cytaty.txt').read()
+# text = open('cytaty.txt').read()
+# print(text)
+
+#zad2
+text = open('cytaty.txt','r').read()
+# text.encode='utf-8'
 print(text)
+# ilosc_wierszy = len(open('cytaty.txt').readlines())
+lines = text.split('\n')
+# print(lines)
+
+for i in range(len(lines)):
+    if i%2!=0:
+       print(lines[i])
