d6a392a1f0159431cc2b7ba9d3db120fd5556ed9
beactivera <weronika.wdowiak56@gmail.com>
2019-11-25 17:25:57 +0100

    zadanie 4

--- 7_pliki.py
+++ 7_pliki.py
@@ -18,15 +18,29 @@ import linecache
 #        print(lines[i])
 
 #zad3
-text = open('cytaty.txt','r').read()
-# text.encode='utf-8'
-# print(text)
-# ilosc_wierszy = len(open('cytaty.txt').readlines())
-lines = text.split('\n')
-# print(lines)
+# text = open('cytaty.txt','r').read()
+# # text.encode='utf-8'
+# # print(text)
+# # ilosc_wierszy = len(open('cytaty.txt').readlines())
+# lines = text.split('\n')
+# # print(lines)
 
-a = int(input("podaj poczatek zakresu: "))
-b = int(input("podaj koniec zakresu: "))
+# a = int(input("podaj poczatek zakresu: "))
+# b = int(input("podaj koniec zakresu: "))
 
-for i in range(a-1,b+1):
-    print(lines[i])
+# for i in range(a-1,b+1):
+#     print(lines[i])
+
+#zad4
+
+lista = []
+nowy_tekst = input("podaj tekst do wpisania: ")
+lista.append(nowy_tekst)
+nowy_tekst2 = input("podaj tekst do wpisania: ")
+lista.append(nowy_tekst2)
+print(lista)
+open('nowy_plik.txt', 'w').writelines(lista)
+
+
+
+
