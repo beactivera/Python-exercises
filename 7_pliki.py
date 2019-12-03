4dfc0dfcef517316fc14c4e36525f1feef234250
beactivera <weronika.wdowiak56@gmail.com>
2019-12-03 08:46:16 +0100

    drugie rozwiazanie zadania 5

--- 7_pliki.py
+++ 7_pliki.py
@@ -61,6 +61,23 @@ import string
 # plik = open('nieskonczonosc.txt', 'a')
 # plik.writelines(lista)
 # plik.close()
+# plik = open('nieskonczonosc.txt', 'r')
+# print(plik.read())
+# plik.close()
+
+#drugi sposob
+lista = []
+nowy_tekst = input("podaj tekst do wpisania: ")
+lista.append(nowy_tekst)
+nowy_tekst2 = input("podaj tekst do wpisania: ")
+lista.append(nowy_tekst2)
+print(lista)
+plik = open('nieskonczonosc.txt', 'a+')
+plik.writelines(lista)
+plik.seek(0)
+print(plik.read())
+plik.close()
+
 
 
 #zad6
@@ -74,6 +91,47 @@ import string
 # open('nowy_plik_zad7.txt', 'w').write(plik)
 
 
+#zad8
+# with open('nieskonczonosc.txt') as f: 
+#     for line in f: 
+#         print(line)
+# plik = open('nieskonczonosc.txt','r').read()
+# encoding = 'UTF-8'  # polskie znaki - czesc znakow
+# words = plik.split(' ')
+# print(words)
+# for i in words:
+#     if words[i]>words[i+1]:
+#         print(words[i],len(words[i]))
+#     else:
+#         print(words[i+1],len(words[i+1]))
 
+
+# Python
+# def longest_words(filename):
+#     with open(filename, 'r') as infile:
+#         words = infile.read().split()
+#     max_len = len(max(words, key=len))
+#     return [word for word in words if len(word) == max_len]
+
+# print(longest_words('about.txt'))
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# def longest_words(filename):
+#     with open(filename, 'r') as infile:
+#         words = infile.read().split()
+#     max_len = len(max(words, key=len))
+#     return [word for word in words if len(word) == max_len]
+ 
+# print(longest_words('about.txt'))
+# # szukam=find(tekst,'ala ma kota')
+# # if szukam>-1:
+# #    print "znalazlem"
+# # else:
+# #    print "nie znalazlem."
 
 
