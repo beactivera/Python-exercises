b7122dba83a3213e720cb2fcfde6cf812aa1119a
beactivera <weronika.wdowiak56@gmail.com>
2019-12-17 09:05:57 +0100

    zadanie 4

--- 8_funkcje.py
+++ 8_funkcje.py
@@ -16,6 +16,7 @@ import random
 # wynik = f(a)-3*f(b)+f(c)
 # print(wynik)
 
+
 #zad2
 # def suma(lista):
 #     suma = 0
@@ -37,16 +38,31 @@ import random
 # print(suma(lista))
 # print(iloczyn(lista))
 
+
 #zad3
-def max_z_listy(lista):
-    return max(lista)
+# def max_z_listy(lista):
+#     return max(lista)
 
-lista = []
-n = int(input('Podaj ile elementow w liscie: '))
+# lista = []
+# n = int(input('Podaj ile elementow w liscie: '))
 
-for i in range(n):
-    lista.append(math.sin(i))
-# print(lista)
+# for i in range(n):
+#     lista.append(math.sin(i))
+# # print(lista)
 
-maxlist = max_z_listy(lista)
-print(maxlist)
+# maxlist = max_z_listy(lista)
+# print(maxlist)
+
+
+#zad4
+def usuwanie(znaki):
+    vowel = ["a","e","o","i"]
+    for i in znaki:
+        if i in vowel:
+            znaki = znaki.replace(i," ")
+    return znaki
+
+lancuch = input("Podaj lancuch znakow: ")
+
+nowy_lancuch = usuwanie(lancuch)
+print(nowy_lancuch)
